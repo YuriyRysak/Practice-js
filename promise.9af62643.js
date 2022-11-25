72e3bab98e8ae8d3f363fbaf8b715e7c39c6de18
YuriyRysak <95884807+YuriyRysak@users.noreply.github.com>
2022-11-25 20:18:22 +0000

    Deploying to gh-pages from @ YuriyRysak/Practice-js@b8ed4c43ca4de6f2c67a35b38014ad777ffef9eb 🚀

---
+++ promise.9af62643.js
@@ -0,0 +1,2 @@
+new Promise((function(e,n){var o=Math.random()>.5;setTimeout((function(){o&&e("Промис выполнился успешно, с результатом (исполнен, fulfilled)"),n("Promis failed (rejected)!")}),1e3)})).then((function(e){console.log(e)})).then((function(e){console.log(e)}));
+//# sourceMappingURL=promise.9af62643.js.map
