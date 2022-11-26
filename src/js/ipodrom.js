b43c30f200e9ec8704713d31c46eb0575baab01a
YuriyRysak <yuriy.risak@gmail.com>
2022-11-26 21:56:11 +0200

    upd rpodrom

--- src/js/ipodrom.js
+++ src/js/ipodrom.js
@@ -1,24 +1,24 @@
-// import '../css/common.css';
+import '../css/common.css';
 
-// const horses = [
-//     'Secretariat',
-//     'Eclipse',
-//     'West Australian',
-//     'Flying Fox',
-//     'Seabiscuit',
-// ];
+const horses = [
+    'Secretariat',
+    'Eclipse',
+    'West Australian',
+    'Flying Fox',
+    'Seabiscuit',
+];
 
-// console.log(
-//     '% 😡  Заезд начася, ставки не принимаютяся!',
-//     'color: brown; font-size: 14px;',
-// );
-// console.log(
-//     `% 😋 Победил ${1}, финишировал за ${1} времени`,
-//     'color: green; font-size: 14px;',
+console.log(
+    '% 😡  Заезд начася, ставки не принимаютяся!',
+    'color: brown; font-size: 14px;',
+);
+console.log(
+    `% 😋 Победил ${1}, финишировал за ${1} времени`,
+    'color: green; font-size: 14px;',
 
-// );
-// console.log(
-//     `% 😁 Заезд окончен, принимаются ставки.`,
-//     'color: blue; font-size: 14px;',
+);
+console.log(
+    `% 😁 Заезд окончен, принимаются ставки.`,
+    'color: blue; font-size: 14px;',
 
-// );
+);
