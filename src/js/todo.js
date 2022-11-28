d27f8d7a9d672649dc1f712f0b336dfebcc8baae
YuriyRysak <yuriy.risak@gmail.com>
2022-11-28 17:29:56 +0200

    required-todo

--- src/js/todo.js
+++ src/js/todo.js
@@ -4,10 +4,59 @@
 //     Завдання можна позначити як виконане
 //     завдання можна видалити
 // Алгоритм
-//     Зверстати форму для ввода нового завдання (інпут)
-//     Зверстати список завдань (текстове значення, кнопка видалення, кнопка виконання)
-//     Повішати подію нажаття на ентер в полі вводу завдання
-//     Записати нове завдання в локал сторідж по кліку на ентер в полі вводу
-//     оновити список завдань в HTML
-//     по кліку на кнопку виконано, закреслити завдання
-//     по кліку на кнопку видалити, видалити завдання
+//    1 Зверстати форму для ввода нового завдання (інпут)
+//    2 Зверстати список завдань (текстове значення, кнопка видалення, кнопка виконання)
+//    3 Повішати подію нажаття на ентер в полі вводу завдання
+//    4 Записати нове завдання в локал сторідж по кліку на ентер в полі вводу
+//    5 оновити список завдань в HTML
+//    6 по кліку на кнопку виконано, закреслити завдання
+//    7 по кліку на кнопку видалити, видалити завдання
+
+const taskInputRef = document.querySelector('#newTask');
+const listRef = document.querySelector('.todo-list');
+const clearBtnRef = document.querySelector('#clearList');
+
+
+taskInputRef.addEventListener('keydown', e => {
+    if(e.key !== 'Enter') return 
+
+    let data = JSON.parse(localStorage.getItem('todoList'));
+
+    if (!data) {
+        data = []
+    }
+
+    data.push(e.target.value);
+
+    const jsonData = JSON.stringify(data);
+
+    localStorage.setItem('todoList', jsonData);
+    e.target.value = '';
+   
+    updateTodoList ()
+
+});
+clearBtnRef.addEventListener('click', e => {
+    localStorage.setItem('todoList', '[]');
+    updateTodoList()
+
+})  
+const updateTodoList = () => {
+    const todoList = JSON.parse(localStorage.getItem('todoList'));
+    listRef.innerHTML = '';
+
+    todoList.forEach((task, index) => {
+        listRef.insertAdjacentHTML('beforeend',`
+        <li class="task-item">
+        <span>${task}</span>
+        <input type="button" value="готово">
+        <input type="button" data-taskId=${index}value="dellet">
+        </li>`
+        )
+     
+
+    })
+  
+
+}
+updateTodoList ();
