7856ce8a73b92ac3e5375a2909cd88c3b4a99e86
YuriyRysak <yuriy.risak@gmail.com>
2022-12-01 12:30:15 +0200

    upd-app

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,3 +1,4 @@
+// import '../css/todo.css';
 // Створити ToDo List
 //     Має бути поле ввода нового завдання
 //     має бути список створених завдань
@@ -26,7 +27,10 @@ taskInputRef.addEventListener('keydown', e => {
         data = []
     }
 
-    data.push(e.target.value);
+    data.push({
+        value: e.target.value,
+        state: 'pending'
+    });
 
     const jsonData = JSON.stringify(data);
 
@@ -40,23 +44,53 @@ clearBtnRef.addEventListener('click', e => {
     localStorage.setItem('todoList', '[]');
     updateTodoList()
 
-})  
+}) 
+listRef.addEventListener('click', e => {
+  if(e.target.className === 'deleteTask') {
+    deleteTask(e.target.dataset.taskid) 
+  }
+
+  if(e.target.className === 'confirmTask') {
+    completeTask(e.target.dataset.taskid)
+  }
+
+}) 
 const updateTodoList = () => {
     const todoList = JSON.parse(localStorage.getItem('todoList'));
     listRef.innerHTML = '';
 
-    todoList.forEach((task, index) => {
+    todoList.forEach(({value, state}, index) => {
         listRef.insertAdjacentHTML('beforeend',`
         <li class="task-item">
-        <span>${task}</span>
-        <input type="button" value="готово">
-        <input type="button" data-taskId=${index}value="dellet">
+        <span class"${state}>${value}</span>
+        <input type="button" class="confirmTask" data-taskID=${index} value="ready">
+        <input type="button" class="deleteTask" data-taskId=${index} value="delete">
         </li>`
         )
-     
+   
 
     })
-  
+  }
+
+const deleteTask = (id) => {
+   
+   const data = JSON.parse(localStorage.getItem('todoList'));
+   data.splice(id, 1);
+   localStorage.setItem('todoList',JSON.stringify(data))
+
+   updateTodoList()
 
 }
-updateTodoList ();
+const completeTask = (id) => {
+    const data = JSON.parse(localStorage.getItem('todoList'));
+
+    data[id] = {
+        ...data[id],
+        state: 'done'
+    }
+
+    localStorage.setItem('todoList',JSON.stringify(data))
+    updateTodoList()
+
+}
+updateTodoList ()
