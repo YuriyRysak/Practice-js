9add8965059d824eb4239ff7bf14ca6336ee4abe
YuriyRysak <yuriy.risak@gmail.com>
2022-12-12 14:02:03 +0200

    news-pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,78 +1,115 @@
 const API_KEY = '1d289197590b4695af2157c664a54f22';
 const PageSize = 8;
-let currentPage =1;
+
+let currentPage = 1;
+let totalPages = undefined;
 
 const searchBtnRef = document.getElementById('searchControl');
+const loadMoreBtnRef = document.getElementById('loadMore');
 const searchField = document.getElementById('searchNameField');
 const articlesContainer = document.getElementById('articles');
 const paginationContainer = document.getElementById('pagination');
 
 searchBtnRef.addEventListener('click', e => {
-     getNewsAxios({
-        query: searchField.value
+     getNews(searchField.value).then(({articles, totalResults}) => {
+        calculatePagination(totalResults);
+        const elements = createArticlesElements(articles);
+        const pageElements = createPagesElements();
 
-    })
+        render(paginationContainer, pageElements);
+        render(articlesContainer, elements);
+     });
+
+    
 });
 
-// function getNews ({query}) {
-//     const urlAPI = `https://newsapi.org/v2/everything?q=${query}&from=2022-12-06&to=2022-12-06&sortBy=popularity&apiKey=${API_KEY}`;
+paginationContainer.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.className !== "page-link") return
 
-//     fetch(urlAPI).then(res => {
-//         if(!res.ok) {
-//             throw new Error(res.message)
-//         }
-//         return res.json()
-//     })
-//     .then(({articles}) => {
-//         render (articles)         
-//     })
-// }
-function calculatePagination(totalResults) {
-    totalPages = Math.ceil(totalResults / PageSize)
-    renderPagination()
+    currentPage = e.target.dataset.page;
 
-}
-function renderPagination() {
-    console.log(totalPages)
+    getNews(searchField.value).then(({articles, totalResults}) => {
+        calculatePagination(totalResults)
 
-}
+        const elements = createArticlesElements(articles);
+        const pageElements = createPagesElements();
 
+        render(paginationContainer, pageElements);
+        render(articlesContainer, elements);
 
-function getNewsAxios ({query}) {
+    });
+})
+
+loadMoreBtnRef.addEventListener('click', e => {
+    if (currentPage > totalPages) return
+
+    currentPage = Number(currentPage) + 1;
+
+    getNews(searchField.value).then(({articles}) => {
+      const elements = createArticlesElements(articles);
+      articlesContainer.insertAdjacentHTML('beforeend', elements);
+    });
+})
+
+function getNews (query) {
     const urlAPI = `https://newsapi.org/v2/everything?q=${query}&from=2022-12-01&apiKey=${API_KEY}&pageSize=${PageSize}&page=${currentPage}`;
 
-    axios.get(urlAPI)
+    return axios.get(urlAPI)
        .then(res => res.data)
        .then(({articles, totalResults}) => {
-        render(articles)
-        calculatePagination(totalResults)
+        return {articles, totalResults}
        })
        .catch(error => console.log(error))
 }
 
-    function render (articles) {
-        articlesContainer.innerHTML = '';
+function createArticlesElements (articles) {
+    return articles.map(({title, description, url, urlToImage}) => {
+      return `<article class="news">
+      <h2>${title}</h2>
+      <div>${description}</div>
+      <img src="${urlToImage}" alt="${title}" loading="lazy">
+      <a href="${url}" class="news-link" target="_blank">more</a>
+    </article>`;
+    }).join('');
+  }
 
-    //     articles.forEach(({title, description, url, urlToImage}) => {
-    //        const articleEl = `<article class="news">
-    //     <h2>${title}</h2>
-    //     <div>${description}</div>
-    //     <img src="${urlToImage}" alt="${title}">
-    //     <a href="${url}">more</a>
-    //   </article>`;
-    //   articlesContainer.insertAdjacentHTML('beforeend', articleEl)
-    //     });
+// function calculatePagination(totalResults) {
+//     totalPages = Math.ceil(totalResults / PageSize)
+//     renderPagination()
 
+// }
 
-       const articlesElements = articles.map(({title, description, url, urlToImage}) => {
-           return `<article class="news">
-         <h2>${title}</h2>
-         <div>${description}</div>
-         <img src="${urlToImage}" alt="${title}">
-         <a href="${url}">more</a>
-       </article>`;
-       
-         });
-         articlesContainer.insertAdjacentHTML('beforeend', articlesElements.join(''));
-        
+/**
+ * createPagesElements
+ * @returns {string} Рядок з елементами кнопок пагінації
+ */
+function createPagesElements () {
+    let pagesElements = '';
+    
+    for (let i = 1; i <= totalPages; i += 1 ) {
+      pagesElements += `<li class="page-item"><a class="page-link" href="#" data-page=${i}>${i}</a></li>`
     }
+  
+    return pagesElements
+  }
+  
+  /**
+   * calculatePagination. Підраховує кількість сторінок
+   * @param {Number} totalResults 
+   */
+  function calculatePagination(totalResults) {
+    totalPages = Math.floor((totalResults > 100 ? 100 : totalResults) / PageSize)
+  }
+  
+  /**
+   * Очищає контейнер і вставляє туди новий контент
+   * @param {HTMLElement} container 
+   * @param {String} content 
+   */
+  function render (container, content) {
+    container.innerHTML = '';
+    container.insertAdjacentHTML('beforeend', content)
+  }
+
+
